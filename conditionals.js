da7bcdcbb0e51788ff15c8da6e6b86023f1dd252
Dante Cross <dantecross@outlook.com>
2020-09-25 06:01:07 +0000

    additional examples

--- conditionals.js
+++ conditionals.js
@@ -1,12 +1,3 @@
-
-const tempreture=12;
-
-
-if(tempreture<0){
-console.log("Make sure you pick out a scarf!");
-}else if(tempreture<15){
-console.log("Short sleeves won't cut it!");
-}else{
-console.log("Short sleeves are fine.");
+if (temperature<-40 ||temperature>40){
+console.log("Maybe going outside isn't such a great idea...");
 }
-console.log("Now you're ready to go outside!");
